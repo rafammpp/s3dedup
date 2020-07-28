96f2d9310c7896ef43951c68e2f542cc1419b1d1
rafammpp <rafammpp@gmail.com>
2020-07-28 13:44:52 +0200

    fixes

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # s3dedup
 Trying to remove duplicate images inside a s3 bucket.
+
+## Requirements
+[https://github.com/jhnc/findimagedupes](https://github.com/jhnc/findimagedupes)
--- image-dedup.py
+++ image-dedup.py
@@ -7,7 +7,6 @@ abs_path = os.path.dirname(os.path.abspath(__file__))
 os.system('findimagedupes -f fingerprints images > dupes.txt')
 
 image_dupes_path = os.path.join(abs_path, 'dupes.txt')
-print('dupes.txt path: ', image_dupes_path)
 dupe_list = []
 url_translate = {}
 dedup_dir = './images_dedup/'
@@ -18,7 +17,6 @@ with open(image_dupes_path) as file:
         image_list = line.split(' ')
         image_list = [(i.strip('\n'), Path(i.strip('\n')).stat().st_size) for i in image_list]
         image_list.sort(key=lambda tup: tup[1], reverse=True)
-        print(image_list)
         dupe_list += [i[0] for i in image_list[1:]]
         for image in image_list:
             url_translate[image[0]] = image_list[0][0]
@@ -28,7 +26,7 @@ with os.scandir(os.path.join(abs_path, 'images/')) as directory:
         if file.is_file() and file.path not in dupe_list:
             if not os.path.exists(dedup_dir):
                 os.makedirs(dedup_dir)
-            shutil.copy(file.path, dedup_dir+file.name)
+            shutil.copy2(file.path, dedup_dir+file.name)
 
 
 with open('url_translate.json', 'w') as file:
